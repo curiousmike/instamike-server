e468a1f75485ef81374efbe9a35af0e3ef77843c
Michael Coustier <curiousmike@gmail.com>
2023-08-03 17:11:41 -0700

    Moved to digitalOcean host - removed FTP for file storage

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # instamike-server
 
 This should be a mongodb.
-To start: node script.js OR nodemon script.js
+To start: node server.js OR nodemon server.js
+
+Note: Images are saved locally
+
+Root path =  binary/user/NightOwlHiker
+
+Root path and fileName created =  binary/user/NightOwlHiker/1691104727324-image.jpg
+
+Fully written to  ../public/instamike/binary/user/NightOwlHiker/1691104727324-image.jpg
+Post created.
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+
 const express = require("express");
+const fs = require('fs')
 const path = require("path");
 const app = express();
 const bodyParser = require("body-parser");
@@ -6,14 +8,13 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const UserModel = require("./models/UserModel");
 const PostModel = require("./models/PostModel");
-const jsftp = require("jsftp"); // for ftp transfer
 const sharp = require("sharp"); // for image resizing
 //
 //
 // SETUP
 //
 //
-const mongoAtlasPassword = "TsYSzfJ7Y98N2Ew";
+const mongoAtlasPassword = "PmqueGSjn7uE8";
 mongoose
   .connect(
     `mongodb+srv://mcoustier:${mongoAtlasPassword}@cluster0.1mm3m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
@@ -22,7 +23,7 @@ mongoose
     const port = 4000;
     const ipAddress = "127.0.0.1";
     app.listen(port, ipAddress, () => {
-      console.log(`Server up ${ipAddress}:${port}`);
+      console.log(`Server up ${ipAddress}:${port}\n\n------------------\n`);
     });
     console.log("connected to mongoDB");
   })
@@ -33,22 +34,7 @@ mongoose
 app.use("/", express.static(path.resolve(__dirname, "assets")));
 app.use(bodyParser.json({ limit: "7mb" }));
 
-//
-// FTP Setup
-//
-//
-// FTP setup
-//
-const FTP = new jsftp({
-  host: "www.coustier.com",
-  port: 21,
-  user: "couszmiz",
-  pass: "Tigerwoods1729!",
-});
-
-const ftpUserPostRootPath = "/public_html/instamike/binary/user/posts";
 const ftpUserAvatarRootPath = "/public_html/instamike/binary/user/avatar";
-const ftpUserPostRootPathSimple = "/instamike/binary/user/posts";
 const ftpUserAvatarRootPathSimple = "/instamike/binary/user/avatar";
 
 //
@@ -61,7 +47,6 @@ const ftpUserAvatarRootPathSimple = "/instamike/binary/user/avatar";
 app.post("/api/modify/user", async (req, res) => {
   const oldUser = req.body.oldData;
   const updatedUser = req.body.updatedData;
-  console.log("\n\n\nMODIFY user =", oldUser.name, oldUser._id, updatedUser);
   const response = await UserModel.findOneAndUpdate(
     {
       _id: oldUser._id,
@@ -107,57 +92,72 @@ app.get("/api/get", async (req, res) => {
 
 // _C_RUD - Create USER
 app.post("/api/create/user", async (req, res) => {
-  console.log("\n\n\nCREATE user ");
+  console.log("\n\n\nCREATE user ", req.body);
   const record = req.body;
 
   // setup FTP path for this new user
-  const yourPath = `${ftpUserAvatarRootPath}/${record.name}`;
-  FTP.raw("mkd", yourPath, (err, folderResultData) => {
+  const yourPath = `/binary/user/${record.name}`
+  console.log('\n\ncreate user = ', record.name)
+  console.log('path = ', yourPath)
+  // const yourPath = `${ftpUserAvatarRootPath}/${record.name}`;
+  fs.mkdir('../instamike/public' + yourPath, (err) => {
     if (err) {
-      console.error(err); // hopefully just "already exists"
+        return console.error('create user create his folder error = ',err);
     }
-    console.log(folderResultData.text); // Show the FTP response text to the user
-    console.log(folderResultData.code); // Show the FTP response code to the user
-    //
-    // copy avatar to ftp
-    let data = Buffer.from(record.avatar, "base64");
-    let finalData = new Buffer.alloc(data.length - 15);
-    data.copy(finalData, 0, 15, data.length); // Mystery - for some reason, when doing the buffer.from base64 above, I get an extra 15 bytes at beginning of result. Strip those here.
-    const yourPathSimple = `${ftpUserAvatarRootPathSimple}/${record.name}`
-    const uploadPathPlusFilename = `${yourPathSimple}/avatar.jpg`;
-    const uploadPathPlusFilenameMedium = `${yourPathSimple}/avatar-medium.jpg`;
-    const uploadPathPlusFilenameSmall = `${yourPathSimple}/avatar-small.jpg`;
-    record.avatarFileName = uploadPathPlusFilename;
-    record.avatarFileNameMedium = uploadPathPlusFilenameMedium;
-    record.avatarFileNameSmall = uploadPathPlusFilenameSmall;
-
-    const ftpUploadPathPlusFilename = `${ftpUserAvatarRootPath}/${record.name}/avatar.jpg`;
-    const ftpUploadPathPlusFilenameMedium = `${ftpUserAvatarRootPath}/${record.name}/avatar-medium.jpg`;
-    const ftpUploadPathPlusFilenameSmall = `${ftpUserAvatarRootPath}/${record.name}/avatar-small.jpg`;
-
-    const smallSize = 256;
-    const mediumSize = 512;
-    sharp(finalData)
-      .resize(smallSize)
-      .toBuffer()
-      .then((smallData) => {
-        FTP.put(smallData, ftpUploadPathPlusFilenameSmall, () => {
-        sharp(finalData)
-          .resize(mediumSize)
-          .toBuffer()
-          .then((mediumData) => {
-            FTP.put(mediumData, ftpUploadPathPlusFilenameMedium, () => {
-              FTP.put(finalData, ftpUploadPathPlusFilename); // blindly upload the fullsize
-              doUserCreate(record, res, uploadPathPlusFilename, uploadPathPlusFilenameSmall, uploadPathPlusFilenameMedium);
-            });
-          });
-        });
-      });
+    console.log('Directory created successfully!');
   });
+  //
+  // 
+  let data = Buffer.from(record.avatar, "base64");
+  let finalData = new Buffer.alloc(data.length - 15);
+  data.copy(finalData, 0, 15, data.length); // Mystery - for some reason, when doing the buffer.from base64 above, I get an extra 15 bytes at beginning of result. Strip those here.
+  const yourPathSimple = yourPath 
+  const fileName = `${yourPathSimple}/avatar.jpg`;
+  const fileNameSmall = `${yourPathSimple}/avatar-medium.jpg`;
+  const fileNameMedium = `${yourPathSimple}/avatar-small.jpg`;
+  record.avatarFileName = fileName;
+  record.avatarFileNameMedium = fileNameMedium;
+  record.avatarFileNameSmall = fileNameSmall;
+
+console.log('fileName  ', fileName)
+console.log('fileNameSmall = ', fileNameSmall)
+console.log('fileNameMedium = ', fileNameMedium)
+  fs.writeFile("../instamike/public/" + fileName, finalData, (err) => {
+    if (err) {
+      console.log('Avatar error writing = ', err)
+    } else {
+      console.log('Avatar Original quality imagefile written success!')
+    }
+  })
+  
+  const smallSize = 256;
+  const mediumSize = 512;
+  sharp(finalData).resize(smallSize).toBuffer()
+    .then((smallData) => {
+      fs.writeFile("../instamike/public/" + fileNameSmall, smallData, (err) => {
+        if (err) {
+          console.log('Avatar error writing = ', err)
+        }
+      })
+    });
+
+  sharp(finalData).resize(mediumSize).toBuffer()
+    .then((mediumData) => {
+      fs.writeFile("../instamike/public/"+fileNameMedium, mediumData, (err) => {
+        if (err) {
+          console.log('Avatar error writing = ', err)
+        } else {
+          console.log('Avatar medium imagefile written success!')
+        }
+      })            
+  });
+
+  doUserCreate(record, res, fileName, fileNameSmall, fileNameMedium);
 
 });
 
 const doUserCreate = async (record, res, avatarFileName, avatarFileNameSmall, avatarFileNameMedium) => {
+  console.log('\n\ndoUserCreate !  ')
   await UserModel.create(record);
   res.json({
     status: 200,
@@ -171,7 +171,6 @@ const doUserCreate = async (record, res, avatarFileName, avatarFileNameSmall, av
 //
 //
 app.get("/api/get/posts", async (req, res) => {
-  console.log("\n\n\nGET posts ");
   const records = await PostModel.find({}).sort({ timeStamp: -1 });
   res.json(records);
 });
@@ -183,51 +182,51 @@ app.post("/api/create/post", async (req, res) => {
   const timeStamp = Date.now();
 
   const record = req.body;
-  console.log("\n\n\nCREATE post - timestamp - ", Date.now(), record.name, record.description);
-  const yourPath = `${ftpUserPostRootPathSimple}/${record.name}`;
-  const fileName = `${yourPath}/${timeStamp}-image.jpg`;
-  const fileNameMedium = `${yourPath}/${timeStamp}-image-medium.jpg`;
-  const fileNameSmall = `${yourPath}/${timeStamp}-image-small.jpg`;
-  record.fileName = fileName; // is it really a jpg?
-  record.fileNameMedium = fileNameMedium;
-  record.fileNameSmall = fileNameSmall;
-
-  // setup FTP path for this user
-  const newFolderPath = `${ftpUserPostRootPath}/${record.name}`;
-  FTP.raw("mkd", newFolderPath, (err, folderDataResult) => {
-    if (err) {
-      console.error(err);
-    }
-    console.log(folderDataResult.text); // Show the FTP response text to the user
-    console.log(folderDataResult.code); // Show the FTP response code to the user
-    //
-    // copy to ftp
+    const yourPath = `binary/user/${record.name}`
+    console.log('your path = ', yourPath)
+    const fileName = `${yourPath}/${timeStamp}-image.jpg`;
+    const fileNameMedium = `${yourPath}/${timeStamp}-image-medium.jpg`;
+    const fileNameSmall = `${yourPath}/${timeStamp}-image-small.jpg`;
+    record.fileName = fileName; // is it really a jpg?
+    record.fileNameMedium = fileNameMedium;
+    record.fileNameSmall = fileNameSmall;
+  
     let data = Buffer.from(record.image, "base64");
     let finalData = new Buffer.alloc(data.length - 15);
     data.copy(finalData, 0, 15, data.length); // Mystery - for some reason, when doing the buffer.from base64 above, I get an extra 15 bytes at beginning of result. Strip those here.
-    const uploadPathPlusFilename = `${ftpUserPostRootPath}/${record.name}/${timeStamp}-image.jpg`;
-    const uploadPathPlusFilenameMedium = `${ftpUserPostRootPath}/${record.name}/${timeStamp}-image-medium.jpg`;
-    const uploadPathPlusFilenameSmall = `${ftpUserPostRootPath}/${record.name}/${timeStamp}-image-small.jpg`;
-
+    console.log('fileName created = ', record.fileName)
+    console.log('and fully written to ', "../instamike/public/" + fileName)
+    fs.writeFile("../instamike/public/" + fileName, finalData, (err) => {
+      if (err) {
+        console.log('error writing = ', err)
+      } else {
+        console.log('Original quality imagefile written success!')
+      }
+    })
+    
     const smallSize = 256;
     const mediumSize = 512;
-    sharp(finalData)
-      .resize(smallSize)
-      .toBuffer()
+    sharp(finalData).resize(smallSize).toBuffer()
       .then((smallData) => {
-        FTP.put(smallData, uploadPathPlusFilenameSmall, () => {
-        sharp(finalData)
-          .resize(mediumSize)
-          .toBuffer()
-          .then((mediumData) => {
-            FTP.put(mediumData, uploadPathPlusFilenameMedium, () => {
-              FTP.put(finalData, uploadPathPlusFilename); // blindly upload the fullsize
-              return doPostCreate(res, record, fileName, fileNameSmall, fileNameMedium);
-            });
-          });
-        });
+        fs.writeFile("../instamike/public/" + fileNameSmall, smallData, (err) => {
+          if (err) {
+            console.log('error writing = ', err)
+          }
+        })
       });
-  });
+
+    sharp(finalData).resize(mediumSize).toBuffer()
+      .then((mediumData) => {
+        fs.writeFile("../instamike/public/"+fileNameMedium, mediumData, (err) => {
+          if (err) {
+            console.log('error writing = ', err)
+          } else {
+            console.log('medium imagefile written success!')
+          }
+        })            
+    });
+    console.log('Post created.\n\n')
+    return doPostCreate(res, record, fileName, fileNameSmall, fileNameMedium);
 
 });
 
@@ -241,7 +240,6 @@ const doPostCreate = async (res, record, fileName, fileNameSmall, fileNameMedium
 
 app.post("/api/delete/post", async (req, res) => {
   const record = req.body;
-  console.log("\nDELETE - ", record);
   const response = await PostModel.deleteOne(record);
   console.log("records = ", response);
   res.json({ status: "ok" });
@@ -251,7 +249,6 @@ app.post("/api/delete/post", async (req, res) => {
 app.post("/api/modify/post", async (req, res) => {
   const oldPost = req.body.oldData;
   const updatedPost = req.body.updatedData;
-  console.log("\n\n\nMODIFY post =", updatedPost.comments);
   const response = await PostModel.findOneAndUpdate(
     {
       _id: oldPost._id,
